4fa693c14003f685e731e77ac92b7670abaf30cd
Maria Eduarda <dudaa.mendes1502@gmail.com>
2024-11-21 08:38:58 -0300

    feat: close issue#3 - criação da tela de perfil

---
+++ frontend/app/src/pages/BatePapo/BatePapo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BatePapo = () => {
+  return <h1>BatePapo</h1>
+}
+
+export default BatePapo
---
+++ frontend/app/src/pages/CriaPost/CriaPost.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CriarPost = () => {
+  return <h1>Criar Post</h1>
+}
+
+export default CriarPost
