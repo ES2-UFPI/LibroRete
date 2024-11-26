991fa058da00e7292ce8cb1ead1b26b4921b80cd
ygor <ygorfcmorais@gmail.com>
2024-11-26 01:16:37 -0300

    endpoint user/@username/listas criado

--- backend/librorete-project/api/urls.py
+++ backend/librorete-project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_listas
+from .views import get_listas, get_user_lists_with_books
 
 urlpatterns = [
     path('listas/', get_listas, name='lista'),
+    path('user/<str:username>/listas/', get_user_lists_with_books, name='user-lists-with-books'),
 ]
--- backend/librorete-project/api/views.py
+++ backend/librorete-project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Lista
 from .serializers import ListaSerializer
+from django.db import connection
 
 @api_view(['GET'])
 def get_listas(request):
@@ -9,3 +10,17 @@ def get_listas(request):
         listas = Lista.objects.all()
         serializer = ListaSerializer(listas, many=True)
         return Response(serializer.data)
+    
+def call_procedure(proc_name, params):
+    with connection.cursor() as cursor:
+        cursor.callproc(proc_name, params)
+        results = cursor.fetchall()
+        columns = [col[0] for col in cursor.description]
+        return [dict(zip(columns, row)) for row in results]
+
+@api_view(['GET'])
+def get_user_lists_with_books(request, username):
+    listas = call_procedure('busca_listas_de_um_usuario', [username])
+    for lista in listas:
+        lista['livros'] = call_procedure('busca_livros_por_username', [username])
+    return Response(listas)
