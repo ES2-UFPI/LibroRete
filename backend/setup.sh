d80781d085abb6064909f6db09899783d2f1707f
mancinidsb <guilhermemancinidsb@gmail.com>
2024-12-15 11:56:24 -0300

    close issue#75

---
+++ backend/setup.sh
@@ -0,0 +1,4 @@
+python backend/app/manage.py makemigrations
+python backend/app/manage.py migrate 
+python backend/app/manage.py createsuperuser --noinput 
+python backend/app/manage.py runserver 0.0.0.0:8000
